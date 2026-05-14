460f0190165e92ee13990975279b6c0d11ac3018
Mariele De Bona <marieledebonam@gmail.com>
2026-05-14 19:36:34 -0300

    Add custom login command for Cypress tests

---
+++ cypress/support/commands.js
@@ -0,0 +1,12 @@
+Cypress.Commands.add('login', (email, password) => {
+
+  cy.get('[data-test=username]')
+    .type(email)
+
+  cy.get('[data-test=password]')
+    .type(password)
+
+  cy.get('[data-test=login-button]')
+    .click()
+
+})
