c7440f08ebb67c69db8993adab49454edf491b2a
Mariele De Bona <marieledebonam@gmail.com>
2026-05-14 20:11:07 -0300

    Refactor checkout test to streamline steps

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -1,27 +1,23 @@
 describe('Checkout Flow', () => {
-
   it('should complete checkout successfully', () => {
+    cy.visit('/')
 
-    cy.visit('https://example.com')
+    cy.get('[data-test="username"]').type('standard_user')
+    cy.get('[data-test="password"]').type('secret_sauce')
+    cy.get('[data-test="login-button"]').click()
 
-    cy.get('[data-testid=product-card]')
-      .first()
-      .click()
+    cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
+    cy.get('[data-test="shopping-cart-link"]').click()
+    cy.get('[data-test="checkout"]').click()
 
-    cy.get('[data-testid=add-cart]')
-      .click()
+    cy.get('[data-test="firstName"]').type('Mariele')
+    cy.get('[data-test="lastName"]').type('De Bona')
+    cy.get('[data-test="postalCode"]').type('88800-000')
 
-    cy.get('[data-testid=cart]')
-      .click()
+    cy.get('[data-test="continue"]').click()
+    cy.get('[data-test="finish"]').click()
 
-    cy.get('[data-testid=checkout-button]')
-      .click()
-
-    cy.get('[data-testid=finish-order]')
-      .click()
-
-    cy.contains('Order completed')
-      .should('be.visible')
+    cy.get('[data-test="complete-header"]')
+      .should('contain', 'Thank you for your order!')
   })
-
 })
